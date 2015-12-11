92118e21e7b9af2771db70e981917007e263ceb3
J <jimfinoc@gmail.com>
2015-12-10 20:35:10 -0500

    ping2

--- pingtest.py
+++ pingtest.py
@@ -15,14 +15,14 @@ def lightlevel():
 
 def range():
     range1 = bus.read_byte_data(address, 2)
-        range2 = bus.read_byte_data(address, 3)
-        range3 = (range1 << 8) + range2
-        return range3
+    range2 = bus.read_byte_data(address, 3)
+    range3 = (range1 << 8) + range2
+    return range3
 
 while True:
     write(0x51)
-        time.sleep(0.7)
-        lightlvl = lightlevel()
-        rng = range()
-        print lightlvl
-        print rng
+    time.sleep(0.7)
+    lightlvl = lightlevel()
+    rng = range()
+    print lightlvl
+    print rng
