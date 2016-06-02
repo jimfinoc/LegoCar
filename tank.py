e5e04a212d9b61959e1853bf0fd06070b8b5ec18
J <jimfinoc@gmail.com>
2016-06-02 19:48:40 -0400

    accuracy!

--- tank.py
+++ tank.py
@@ -184,9 +184,9 @@ accel, mag = lsm303.read()
 # Grab the X, Y, Z components from the reading and print them out.
 accel_x, accel_y, accel_z = accel
 mag_x, mag_y, mag_z = mag
-print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
+print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(accel_x, accel_y, accel_z, mag_x, mag_y, mag_z),)
 # Wait half a second and repeat.
 time.sleep(0.5)
 
 heading = (math.atan2(mag_y,mag_x) * 180) / math.pi;
-print('Heading = {0}'.format(heading))
+print('Heading = {:06.2f}'.format(heading))
