c46444b46b9a747b9ea99d042ba6c41c5e05689f
J <jimfinoc@gmail.com>
2016-06-02 19:49:07 -0400

    comma

--- tank.py
+++ tank.py
@@ -184,7 +184,7 @@ accel, mag = lsm303.read()
 # Grab the X, Y, Z components from the reading and print them out.
 accel_x, accel_y, accel_z = accel
 mag_x, mag_y, mag_z = mag
-print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(accel_x, accel_y, accel_z, mag_x, mag_y, mag_z),)
+print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(accel_x, accel_y, accel_z, mag_x, mag_y, mag_z)),
 # Wait half a second and repeat.
 time.sleep(0.5)
 
