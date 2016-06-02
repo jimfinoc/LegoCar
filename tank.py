97d19a15d11a8d61168198f5236d0521ed5f0891
J <jimfinoc@gmail.com>
2016-06-02 19:43:39 -0400

    print it

--- tank.py
+++ tank.py
@@ -189,3 +189,4 @@ print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.f
 time.sleep(0.5)
 
 heading = (math.atan2(mag_y,mag_x) * 180) / math.pi;
+print('Heading = {0}'.format(heading))
