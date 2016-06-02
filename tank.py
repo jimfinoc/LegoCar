0b64db7ad981349d8c635bb935d2a78b4a446a86
J <jimfinoc@gmail.com>
2016-06-02 19:51:01 -0400

    do it

--- tank.py
+++ tank.py
@@ -185,8 +185,7 @@ accel, mag = lsm303.read()
 accel_x, accel_y, accel_z = accel
 mag_x, mag_y, mag_z = mag
 print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(accel_x, accel_y, accel_z, mag_x, mag_y, mag_z)),
-# Wait half a second and repeat.
-time.sleep(0.5)
-
 heading = (math.atan2(mag_y,mag_x) * 180) / math.pi;
 print('Heading = {:03.0f}'.format(heading))
+# Wait half a second and repeat.
+time.sleep(0.5)
